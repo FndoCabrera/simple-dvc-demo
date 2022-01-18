b6c912274b02055ca565f6822b9915f93d5a7fec
Fernando Cabrera <fndo.cabrera92@gmail.com>
2022-01-18 20:52:41 -0300

    setup done

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description = "it is a wine Q package",
+    author = "FernandoC",
+    package = find_packages(),
+    license = "MIT",
+    
+
+)
